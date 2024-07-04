ab43062cab76a14e2231b34e59e84fd7a8e53f9c
Timna Brown <belindabrownr04@gmail.com>
2024-07-04 09:26:59 -0600

    Create prototipo_func.h

---
+++ prototipo_func.h
@@ -0,0 +1,14 @@
+#ifndef PROTOTIPO_FUNC
+#define PROTOTIPO_FUNC
+
+#include "header.h"
+
+
+void geoObtenerMatrizDeRotacionR0();
+double beconvertirDeRadianesAGrados (double angle);
+double geoConvertirDeGradosARadianes (double angle);
+void geoLeerParametrosDeControlDeArchivoDeTexto();
+void geoSalvarResultadosEnArchivoDeTexto();
+
+
+#endif
